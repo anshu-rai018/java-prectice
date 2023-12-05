ae4879ba4af8aea0df30a60e883ca6e116d98f5f
anshu-rai018 <anshu.rai018@gmail.com>
2023-12-05 21:08:18 +0530

    first commit

---
+++ BasicExercisesPart-I/Exercise_1_partOne.java
@@ -0,0 +1,10 @@
+public class Exercise_1_partOne {
+    public static void main(String[] args) {
+        // Calculate the sum of 24 and 26
+        int sum = 24 + 26;
+        // Print the result of the addition
+        System.out.println(sum);
+    }
+}
+
+
