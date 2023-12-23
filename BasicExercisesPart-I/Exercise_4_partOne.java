e5ef4528ebf59c2478b191dc05918520cc985b05
anshu-rai018 <anshu.rai018@gmail.com>
2023-12-24 03:48:48 +0530

    first commit

---
+++ BasicExercisesPart-I/Exercise_4_partOne.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Exercise_4_partOne {
+
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from the user
+        Scanner in =  new Scanner(System.in);
+
+        // prompt the user to input the first number
+        System.out.print("Input first number: ");
+        // Read and store the first number
+        int num1 = in.nextInt();
+
+        // prompt the user to input the second number
+        System.out.print("Input second number: ");
+        // read and store the second number
+        int num2 = in.nextInt();
+
+        // Calculate the product of the two numbers and display the result
+        System.out.println(num1 + " x " + num2 + " = " + num1 * num2 );
+    }
+}
---
+++ BasicExercisesPart-I/Exercise_4_partTwo.java
@@ -0,0 +1,13 @@
+public class Exercise_4_partTwo {
+
+    // Declare and  initialize static variable x and y
+    static int x = 25;
+    static int y = 5;
+
+    public static void main(String [] args){
+
+        // Calculate and print the product of x and y
+        System.out.println(x * y);
+    }
+
+}
