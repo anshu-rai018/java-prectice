522bb99352a9b53caefaaf83a2df2fc021156611
anshu-rai018 <anshu.rai018@gmail.com>
2023-12-30 19:46:58 +0530

    first commit

---
+++ BasicExercisesPart-I/makeJavaPattern.java
@@ -0,0 +1,10 @@
+public class makeJavaPattern {
+
+    public static void main(String[] args) {
+        // Display the characters to form the text "java" ibn a specific pattern
+        System.out.println( "     J     a     v       v      a ");
+        System.out.println( "     J    a  a    v     v     a   a");
+        System.out.println( " J   J   aaaaaa    v   v     aaaaaaa");
+        System.out.println("   jj   a       a    v      a        a");
+    }
+}
