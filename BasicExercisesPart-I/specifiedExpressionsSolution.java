c21294e0c794e81db1c1e10c593cd63dac222404
anshu-rai018 <anshu.rai018@gmail.com>
2023-12-30 20:09:20 +0530

    first commit

---
+++ BasicExercisesPart-I/specifiedExpressionsSolution.java
@@ -0,0 +1,8 @@
+import java.sql.SQLOutput;
+
+public class specifiedExpressionsSolution {
+    public static void main(String[] args) {
+         // Calculate  and print  the  result of a mathematical expression
+        System.out.println((25.5 * 3.5  - 3.5 * 3.5) / (40.5 - 4.5));
+    }
+}
