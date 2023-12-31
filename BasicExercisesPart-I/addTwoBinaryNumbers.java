7a7c1c42d7baf967b605a4f9f7d08b5ef31508fd
anshu-rai018 <anshu.rai018@gmail.com>
2023-12-31 16:49:15 +0530

    first commit

---
+++ BasicExercisesPart-I/addTwoBinaryNumbers.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class addTwoBinaryNumbers {
+    public static void main(String[] args) {
+         // Declare  variables to store two binary  numbers, an index , and  a remainder
+        long binary1, binary2;
+        int i = 0, remainder = 0;
+
+        // Create an array to store the sum of binary digits
+        int[] sum = new int[20];
+
+        // Create  a Scanner object to read input from  the user
+        Scanner in = new Scanner(System.in);
+
+        // Prompt the user to input the first binary number
+        System.out.print("Input first binary number: ");
+        binary1 = in.nextLong();
+
+        // Prompt the user to input the second binary number
+        System.out.print("Input second binary number:  ");
+        binary2 = in.nextLong();
+
+        // Perform binary  addition while there are digits in the binary numbers
+        while (binary1 != 0 || binary2 != )
+        {
+            // Calculate the sum of binary digits and update the remainder
+            sum[i++] = (int)((binary1 % 10 + binary2 % 10 + remainder) % 2);
+            remainder = (int)((binary1 % 10 + binary2 % 10 + remainder) /2);
+            binary1 = binary1 / 10;
+            binary2 = binary2 / 10;
+        }
+
+        // If there is a remaining carry, add it to the sum
+        if (remainder != 0){
+            sum[1++] = remainder;
+        }
+
+        // Decrement the index to prepare for
+    }
+}
